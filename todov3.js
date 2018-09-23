5d4ddf970a9e4b0255000862497d6e6a83f2c7f7
etubom <way2young4u@hotmail.com>
2018-09-23 21:57:13 +0100

    added displayTodos method to todoList obj

--- todov3.js
+++ todov3.js
@@ -8,5 +8,8 @@
  * 
  */
 let todoList = {
-  todos: ["item 1", "item 2", "item 3"]
+  todos: ["item 1", "item 2", "item 3"],
+  displayTodos: function() {
+    console.log("My Todos: ", this.todos);
+  }
 };
