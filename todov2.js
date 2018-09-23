852598a22b9a99280be7c97b24829691cd677afb
etubom <way2young4u@hotmail.com>
2018-09-23 17:47:34 +0100

    Requirements added to file

---
+++ todov2.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ V2 requirements
+ It should have a function to display todos
+ It should have a function to add new todos
+ It should have a function to change todos
+ It should have a function to delete todos 
+ * 
+ */
