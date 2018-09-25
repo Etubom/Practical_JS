07ca22575fdf0e18bb45ab191f546258b8813ab3
etubom <way2young4u@hotmail.com>
2018-09-25 12:22:07 +0100

    addTodo method modified to return an Obj and JS html src file updated

--- todov4.js
+++ todov4.js
@@ -4,3 +4,24 @@
  todoList.changeTodo should change the todoText property
  todoList.toggleCompleted should change the completed property
  */
+let todoList = {
+  todos: [],
+  displayTodos: function() {
+    console.log("My Todos: ", this.todos);
+  },
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function(indexValueToChange, newValue) {
+    this.todos[indexValueToChange] = newValue;
+    this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  }
+};
