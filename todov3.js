24237b4a81676787818ef2e130db7a399cbeda92
etubom <way2young4u@hotmail.com>
2018-09-25 11:39:22 +0100

    Added deleteTodo method to todoList Obj

--- todov3.js
+++ todov3.js
@@ -19,5 +19,9 @@ let todoList = {
   changeTodo: function(indexValueToChange, newValue) {
     this.todos[indexValueToChange] = newValue;
     this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
   }
 };
